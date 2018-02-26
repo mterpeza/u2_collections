1a4203c8951486f469b73702c3ff55be72e98b2e
mterpeza <mterpeza@hotmail.com>
2018-02-25 18:55:11 -0600

    Initial commit

---
+++ src/unit2collections/Main.java
@@ -0,0 +1,14 @@
+package unit2collections;
+
+public class Main {
+
+    // List of IO methods (input/output)
+    private final static fileInput dateDiff = new fileInput("date_diff.csv");
+
+    public static void main(String[] args){
+
+        // date_diff.csv file here (read data)
+        
+
+    }
+}
---
+++ src/unit2collections/fileInput.java
@@ -0,0 +1,4 @@
+package unit2collections;
+
+public class fileInput {
+}
