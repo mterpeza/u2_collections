2ddf842b12763228cc05d5c422cd7b1bbfd55998
mterpeza <mterpeza@gmail.com>
2018-03-04 23:28:15 -0600

    Add files via upload

---
+++ tests/unit2collections/MainTest.java
@@ -0,0 +1,32 @@
+package unit2collections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void main1() {
+    }
+
+    @Test
+    void lineprocessor() {
+    }
+
+    @Test
+    void monthNumber() {
+    }
+
+    @Test
+    void greaterDate() {
+    }
+
+    @Test
+    void calculateDiff() {
+    }
+}
